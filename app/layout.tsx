f367876e8542f6572d739a10e9d963c8e4bba44f
Suhani <suhani@stackaisolutions.com>
2026-06-02 02:36:16 +0530

    feat: add QueryClientProvider and Toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
+import { Toaster } from "sonner";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+          <Providers>
+            {children}
+          </Providers>
+          <Toaster position="top-right" richColors />
+      </body>
     </html>
   );
 }
