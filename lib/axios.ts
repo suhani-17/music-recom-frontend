67a857a13bc9e7d2161146fb1e04577b91bc4239
Suhani <suhani@stackaisolutions.com>
2026-06-02 01:59:41 +0530

    feat: read API base URL from environment variable

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000', // Change this to your backend URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL, // Change this to your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
